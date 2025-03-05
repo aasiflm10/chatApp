b8b21a79eb7aeb92678982ee2525b714b19b9032
aasiflm10 <aa6125405@gmail.com>
2025-03-05 21:54:51 +0530

    websockets and room working

--- backendChatApp/dist/index.js
+++ backendChatApp/dist/index.js
@@ -5,14 +5,32 @@ const wss = new ws_1.WebSocketServer({ port: 8080 });
 let userCount = 0;
 let allSockets = [];
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
     userCount = userCount + 1;
     console.log("user connected #" + userCount);
     socket.on("message", (message) => {
-        console.log("message received " + message.toString());
-        for (let i = 0; i < allSockets.length; ++i) {
-            const s = allSockets[i];
-            s.send("Sent from the server  : " + message.toString());
+        const parsedMessage = JSON.parse(message);
+        console.log(message.toString());
+        console.log(parsedMessage);
+        if (parsedMessage.type === "join") {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.message,
+            });
+        }
+        if (parsedMessage.type === "chat") {
+            // const currentUserRoom = allSockets.find((x)=> x.socket == socket)?.room;
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; ++i) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+            //send the message to all users who are in that room
+            for (let i = 0; i < allSockets.length; ++i) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     });
 });
--- backendChatApp/src/index.ts
+++ backendChatApp/src/index.ts
@@ -2,22 +2,47 @@ import { WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+
 let userCount = 0;
-let allSockets : WebSocket[] = []; 
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  allSockets.push(socket);
-
   userCount = userCount + 1;
   console.log("user connected #" + userCount);
 
   socket.on("message", (message) => {
-    console.log("message received " + message.toString());
-    
-    for(let i = 0 ; i < allSockets.length; ++i){
-      const s = allSockets[i];
-      s.send("Sent from the server  : " + message.toString());
+    const parsedMessage = JSON.parse(message as unknown as string);
+    console.log(message.toString());
+    console.log(parsedMessage);
+
+    if (parsedMessage.type === "join") {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.message,
+      });
+    }
+
+    if (parsedMessage.type === "chat") {
+      // const currentUserRoom = allSockets.find((x)=> x.socket == socket)?.room;
+
+      let currentUserRoom = null;
+      for (let i = 0; i < allSockets.length; ++i) {
+        if (allSockets[i].socket == socket) {
+          currentUserRoom = allSockets[i].room;
+        }
+      }
+
+      //send the message to all users who are in that room
+
+      for (let i = 0; i < allSockets.length; ++i) {
+        if (allSockets[i].room == currentUserRoom) {
+          allSockets[i].socket.send(parsedMessage.payload.message);
+        }
+      }
     }
   });
 });
-
